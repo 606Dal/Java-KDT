026ccd4c663c6e771a19c095042939e24dd3529e
Misyel <ydw317@naver.com>
2025-01-18 10:50:09 +0900

    설명 수정

--- 0114/src/main/java/org/example/vending/VM2.java
+++ 0114/src/main/java/org/example/vending/VM2.java
@@ -3,13 +3,13 @@ package org.example.vending;
 /*
   Enum은 처음부터 몇 개를 만들지 정해야 함.
   - 지정된 숫자만큼의 객체만 생성
-  - 외부에서 바꾸면 안 돼서 public
+  - 외부에서 바꾸면 안 돼서 public X
  */
 public enum VM2 {
 	// 상수로 (없으면 메서드 생성에서 오류남)
 	INSTANCE; // 필요한 갯수만큼 객체 생성.
 
-	// 생성자 사용 못함. public
+	// public 생성자 사용 못함.
 
 	public String hello() {
 		return "Hello";
